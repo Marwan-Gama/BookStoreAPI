d39bd681a2a3279cd944ea5f6d3f1f4f91e4080b
Your GitHub Username <marwanabugama2000@gmail.com>
2024-05-12 17:10:28 +0300

    Error Handler Middleware

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const booksPath = require("./routes/books");
 const authorsPath = require("./routes/authors");
 const authPath = require("./routes/auth");
+const usersPath = require("./routes/users");
 const mongoose = require("mongoose");
 const logger = require("./middlewares/logger");
+const { notFound, errorHandler } = require("./middlewares/errors");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -28,6 +31,11 @@ app.use(logger);
 app.use("/api/books", booksPath);
 app.use("/api/authors", authorsPath);
 app.use("/api/auth", authPath);
+app.use("/api/users", usersPath);
+
+// Error handling
+app.use(notFound);
+app.use(errorHandler);
 
 // Runing the server
 const PORT = process.env.PORT;
