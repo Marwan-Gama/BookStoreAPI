27041c25216a438f68e94f61ebad0001ec7919bc
Your GitHub Username <marwanabugama2000@gmail.com>
2024-05-12 11:20:50 +0300

    auth login user

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const AsyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const {
   User,
   ValidateLoginUser,
   ValidateRegisterUser,
-  ValidateUpdateUser,
 } = require("../models/User");
 
 /**
@@ -27,6 +28,9 @@ router.post(
       return res.status(400).json({ message: "this user already registered" });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+
     user = new User({
       email: req.body.email,
       username: req.body.username,
@@ -34,8 +38,51 @@ router.post(
       isAdmin: req.body.isAdmin,
     });
 
+    const token = null;
     const result = await user.save();
-    res.status(201).json(result);
+    const { password, ...other } = result._doc;
+    res.status(201).json({ ...other, token });
+  })
+);
+
+/**
+ * @desc  Login User
+ * @route /api/auth
+ * @method POST
+ * @access puplic
+ */
+router.post(
+  "/login",
+  AsyncHandler(async (req, res) => {
+    const { error } = ValidateLoginUser(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: "invalid email or password" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: "invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "3d",
+      }
+    );
+
+    const { password, ...other } = user._doc;
+    res.status(200).json({ ...other, token });
   })
 );
 
